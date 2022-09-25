9d77cad90a3143ed49564442af068a9872e23162
Alex <page.alexanderjohn@gmail.com>
2022-09-25 18:54:31 -0400

    Fixed problems and added JQuery code
    Final version, fixed the about button click issue by adding JQuery, also adapted other parts of my JavaScript to JQuery

--- README.txt
+++ README.txt
@@ -1,10 +1,8 @@
-10,000 Hour Calcuator:
+# 10,000 Hour Calcuator
 
-Description: A calculator for determining how long you have left to 
+Description: A calculator for determining how many hours you have left to 
 reach 10,000 hours in a skill. The calculator will tell you
 how many hours you have left based on how many hours a day
-you practice a skill, and how many days a week you practice. It can take into 
-account how long you've been practicing for aswell.
+you practice a skill, and how many days a week you practice.
 
-Made with HTML/CSS, Bootstrap, and JavaScript for CS50's Computer Science 
-for Web Programming final project
+Made with Bootstrap & JQuery
--- calculator.js
+++ calculator.js
@@ -1,16 +1,12 @@
 function calc() {
-    // grabs the result <p> element
-    let result_txt = document.getElementById('result');
-    // grabs the calc_container <p> element
-    let calc_container = document.getElementById('calc_container');
     // gets value from #hours input
-    let hours = Number(document.getElementById('hours').value);
+    let hours = Number($('#hours').val());
     // gets value from #days input
-    let days = Number(document.getElementById('days').value);
+    let days = Number($('#days').val());
     // gets value from #days input
-    let years = Number(document.getElementById('years').value);
+    let years = Number($('#years').val());
     // gets value from #days input
-    let months = Number(document.getElementById('months').value);
+    let months = Number($('#months').val());
     // initializes variables for nested functions
     const weeks = 52.143;
     const ten_k = 10000;
@@ -70,9 +66,11 @@ function calc() {
         console.log('tw: ', total_weeks)
         console.log('days: ', days)
         console.log('hours: ', hours)
+        // grabs the result <p> element
+        let result_txt = document.getElementById('result');
         // if the elements are not visible, they are made visible
-        result_txt.style.display = 'block';
-        calc_container.style.visibility = 'visible';
+        $('#result').show();
+        $('#calc_container').css("visibility", "visible");
         // calculates total hours spent practicing so far
         let to_date = (total_weeks * days) * hours;
         console.log('to date: ', to_date);
@@ -81,20 +79,20 @@ function calc() {
             // calculates surplus
             surplus = to_date - ten_k;
             surplus = Math.round(surplus);
-            result = document.getElementById('result').innerText = `Congrats! You\'ve surpassed 10,000 hours by ${surplus} hours!`;
+            result_txt.innerText = `Congrats! You\'ve surpassed 10,000 hours by ${surplus} hours!`;
             // exit
             return
         }
         else {
             let remaining_hours = Math.round(ten_k - to_date);
             let total = (hours * days) * weeks;
-            result = remaining_hours / total;
+            let result = remaining_hours / total;
             years_result = Math.round(result);
             months_result = Math.round(12 * (result - Math.floor(result)));
 
             // in the case of the users input equaling 10k
             if (Math.round(to_date) == ten_k) {
-                result = document.getElementById('result').innerText = 'You\'ve reached 10,000 hours, time to celebrate!';
+                result_txt.innerText = 'You\'ve reached 10,000 hours, time to celebrate!';
                 console.log('to date: ', to_date);
                 // exit
                 return
@@ -102,7 +100,7 @@ function calc() {
             // otherwise
             else {
                 // takes result and rounds upwards for the first decimal 
-                result = document.getElementById('result').innerText = `You have approx. ${remaining_hours} hours left.\nAt your set pace, It will take about:\n${years_result} Year(s), and ${months_result} month(s)\nto reach 10,000 hours of practice.`;
+                result_txt.innerText = `You have approx. ${remaining_hours} hours left.\nAt your set pace, It will take about:\n${years_result} Year(s), and ${months_result} month(s)\nto reach 10,000 hours of practice.`;
                 // exits function
                 return
             }
@@ -111,23 +109,18 @@ function calc() {
         // runs filter function
         return filter();
 }
-
-
-function about_reveal() {
-    // grabs the about <p> element
-    let about = document.getElementById('about');
-    // grabs the about_container <p> element
-    let abt_container = document.getElementById('about_container');
+// grabs the about <input> element
+$(document).on('click', '#about_btn', function about_reveal() {
     // if the elements are not visible, they are made visible
-    if (about.style.display == 'none' || abt_container.style.display == 'hidden') {
-        about.style.display = 'block';
-        abt_container.style.visibility = 'visible';
-        
+    if ($('about').css('display') == 'none' || $('#about_container').css('visibility') == 'hidden') {
+        $('#about').show();
+        $('#about_container').css("visibility", "visible");
+
     }
+
     // if the elements are visible, they are hidden
     else {
-        about.style.display = 'none';
-        abt_container.style.visibility = 'hidden';
+        $('#about').hide();
+        $('#about_container').css("visibility", "hidden");
     }
-};
-
+});
